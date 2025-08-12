efbb9d614e367774da18847920542aa201b784c2
Michael Hansen <mike@rhasspy.org>
2025-08-12 16:12:04 -0500

    Refactor into single HassSetVolumeRelative intent

--- script/intentfest/validate.py
+++ script/intentfest/validate.py
@@ -289,6 +289,7 @@ SENTENCE_COMMON_SCHEMA = vol.Schema(
                         vol.Required("to"): int,
                         vol.Optional("step", default=1): int,
                         vol.Optional("fractions"): vol.Any("halves", "tenths"),
+                        vol.Optional("multiplier"): vol.Coerce(float),
                     },
                     "wildcard": bool,
                 }
@@ -412,6 +413,7 @@ SHARED_LISTS_SCHEMA = vol.Schema(
                         vol.Optional("step"): int,
                         vol.Optional("type"): "percentage",
                         vol.Optional("fractions"): "halves",
+                        vol.Optional("multiplier"): vol.Coerce(float),
                     }
                 },
                 {vol.Required("wildcard"): bool},
@@ -456,6 +458,7 @@ def LANGUAGE_LISTS_SCHEMA(language: str) -> vol.Schema:
                             vol.Optional("step"): int,
                             vol.Optional("type"): "percentage",
                             vol.Optional("fractions"): "halves",
+                            vol.Optional("multiplier"): vol.Coerce(float),
                         }
                     },
                     {vol.Required("wildcard"): bool},
